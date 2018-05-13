89ddf5fd0bbab31bf8043c193f24ce6126a856a0
Mark <mark.lee1933@gmail.com>
2018-05-13 16:35:13 +0900

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 let socket = io();
 
+function scrollToBottom () {
+  //selectors
+  let messages = jQuery('#messages');
+  let newMessage = messages.children('li:last-child')
+  //heights
+  let clientHeight = messages.prop('clientHeight');
+  let scrollTop = messages.prop('scrollTop');
+  let scrollHeight = messages.prop('scrollHeight');
+  let newMessageHeight = newMessage.innerHeight();
+  let lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function (){
   console.log('Connected to server');
 
@@ -23,11 +39,7 @@ socket.on('newMessage', function (message){
   });
 
   jQuery('#messages').append(html);
-
-  // let li = jQuery('<li></li>');
-  // li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message){
@@ -40,14 +52,7 @@ socket.on('newLocationMessage', function (message){
   });
 
   jQuery('#messages').append(html);
-
-  // let li = jQuery('<li></li>');
-  // let a = jQuery('<a target="_blank">My current location</a>');
-
-  // li.text(`${message.from} ${formattedTime}: `);
-  // a.attr('href', message.url);
-  // li.append(a);
-  // jQuery('#messages').append(li);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e){
